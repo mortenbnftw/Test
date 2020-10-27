dbae7bb744dec2828f97c37c1a4338df26b1bca2
Morten Brandt Nilsson <mbrandtnilsson@hotmail.com>
2020-10-27 20:30:04 +0100

    Removed some code

--- script.py
+++ script.py
@@ -1,14 +1,4 @@
-import math
-import os
-import sys
-
 import requests
-
-# print(sys.version)
-# print(sys.executable)
-def greet(who_to_greet):
-    greeting = "hello, {}".format(who_to_greet)
-    return greeting
 
 
 r = requests.get("https://krak.dk")
